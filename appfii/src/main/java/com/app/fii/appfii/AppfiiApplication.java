eb5d615cfe1c7cf48721df1b37df264390f41eaf
Eduardo Luiz Delmonego <104005418+edueld87@users.noreply.github.com>
2022-09-01 11:28:53 -0300

    Add files via upload

---
+++ appfii/src/main/java/com/app/fii/appfii/AppfiiApplication.java
@@ -0,0 +1,13 @@
+package com.app.fii.appfii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfiiApplication.class, args);
+	}
+
+}
---
+++ appfii/src/test/java/com/app/fii/appfii/AppfiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.fii.appfii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
